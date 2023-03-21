60673a6d5bbec11c320fe954c8d7ff3affecd9eb
Suraj-Accio <124239927+Suraj-Accio@users.noreply.github.com>
2023-03-21 13:10:39 +0530

    Initial commit

---
+++ cypress/integration/tests/test.spec.js
@@ -0,0 +1,16 @@
+/// <reference types="Cypress"/>
+
+describe("test suite name", () => {
+
+  const baseURL = "http://localhost:8080";
+  
+  beforeEach(() => {
+    cy.visit(baseURL);
+  });
+
+  it("test case name", () => {
+
+
+  });
+
+});
